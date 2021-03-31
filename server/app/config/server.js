440c992330821c4ff7996b0afab5ad732c9a7db9
David Castillo <vikruzdavid@gmail.com>
2021-03-31 16:31:44 -0400

    Fix Nginx

--- server/app/config/server.js
+++ server/app/config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  url: env("URL", "http://localhost:1337"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '1f4bb3dd2d78fd4a89e134b728bc9335'),
+      secret: env("ADMIN_JWT_SECRET", "1f4bb3dd2d78fd4a89e134b728bc9335"),
     },
   },
 });
