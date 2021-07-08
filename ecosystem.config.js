8cd6aa4017bd7f0c50229cedf318640ec40404fe
David Castillo <vikruzdavid@gmail.com>
2021-07-08 02:00:20 -0400

    Production Build

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps: [
+    {
+      name: "InventoryX",
+      script: "./node_modules/.bin/nuxt",
+      args: "start",
+      instances: 1,
+      autorestart: true,
+      exp_backoff_restart_delay: 5000,
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: "root",
+      host: "8112b9a.online-server.cloud",
+      ref: "origin/master",
+      repo: "git@github.com:davidcastilloc/StackInventoryX.git",
+      path: "/var/www/html",
+      "pre-deploy": "git fetch --all",
+      "post-deploy":
+        "yarn install && yarn build && pm2 reload ecosystem.config.js --env production",
+    },
+  },
+};
